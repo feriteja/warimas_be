89375c51c35b4f1618de1eb369583b373af9a194
Feri Teja Kusuma <feriteja@gmail.com>
2025-12-24 11:32:52 +0700

    🐛 fix login bug

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -43,7 +43,7 @@ func (r *repository) Create(ctx context.Context, email, password, role string) (
 func (r *repository) FindByEmail(email string) (User, error) {
 	var u User
 	err := r.db.QueryRow(
-		"SELECT u.id, u.email, u.password, u.role, s.id FROM users u JOIN sellers s ON u.id = s.user_id WHERE u.email=$1",
+		"SELECT u.id, u.email, u.password, u.role, s.id FROM users u LEFT JOIN sellers s ON u.id = s.user_id WHERE u.email=$1",
 		email,
 	).Scan(&u.ID, &u.Email, &u.Password, &u.Role, &u.SellerID)
 
