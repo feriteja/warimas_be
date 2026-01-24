af2c1754a97c50e508d75049cf0bb46428b4db33
Feri Teja Kusuma <feriteja@gmail.com>
2026-01-24 14:38:05 +0700

    adjust cors for apply the limiter via header

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -9,7 +9,7 @@ func CORS(next http.Handler) http.Handler {
 
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Device-ID, X-Client-Type, X-Action")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Vary", "Origin")
 
